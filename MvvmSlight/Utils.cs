ea3d369ab23c19ff309f3f21c4fbc197435c30cb
John Burak <john.burak@gmail.com>
2018-01-30 16:04:56 -0700

    Add IsInDesignMode utility.

---
+++ MvvmSlight/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmSlight
+{
+    public static class Utils
+    {
+        /// <summary>
+        ///   Useful for loading design-time values into an view model classes,
+        ///   which keeps Visual Studio's "Go to Definition", etc. working
+        ///   correctly in XAML designer.
+        /// </summary>
+        public static bool IsInDesignMode => isInDesignMode;
+        static bool isInDesignMode = DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject());
+    }
+}
