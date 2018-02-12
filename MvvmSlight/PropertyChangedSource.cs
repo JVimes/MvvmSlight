04aa42b0dbc9cec4b0910258ed145e423b3e9aa4
John Burak <john.burak@gmail.com>
2018-02-12 12:28:12 -0700

    Support "backing properties"

--- MvvmSlight/PropertyChangedSource.cs
+++ MvvmSlight/PropertyChangedSource.cs
@@ -17,21 +17,57 @@ namespace MvvmSlight
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        ///   Call this inside the setter of each property that should fire the
-        ///   <see cref="PropertyChanged"/> event.
+        ///   For <see cref="PropertyChanged"/> firing properties with a
+        ///   traditional backing field, call this inside the setter.
         /// </summary>
-        /// <typeparam name="T"> The type of the property. </typeparam>
+        /// <typeparam name="T"> The data type of the property. </typeparam>
         /// <param name="field"> A ref to the backing field. </param>
         /// <param name="value"> The value to set the property to. </param>
         /// <param name="propertyName">
-        ///   Leave blank to use the property's name, or pass a string to
-        ///   manually specify.
+        ///   Usually leave blank (to use the name of the property whose setter
+        ///   is being called). The property name that will be fired by the <see
+        ///   cref="PropertyChanged"/> event.
         /// </param>
         protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            if (AreEqual(field, value)) return;
             field = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            FireEvent(propertyName);
         }
+
+        /// <summary>
+        ///   For <see cref="PropertyChanged"/> firing properties backed by
+        ///   another property, potentially on another object.
+        /// </summary>
+        /// <param name="backingObject">
+        ///   The object that the backing property is on.
+        /// </param>
+        /// <param name="backingPropertyName">
+        ///   The name of the backing property. Use C#'s "nameof" keyword at
+        ///   call-site, if possible.
+        /// </param>
+        /// <param name="value"> The value to set the property to. </param>
+        /// <param name="propertyName">
+        ///   Usually leave blank (to use the name of the property whose setter
+        ///   is being called). The property name that will be fired by the <see
+        ///   cref="PropertyChanged"/> event.
+        /// </param>
+        protected void Set<T>(object backingObject, string backingPropertyName, T value,
+            [CallerMemberName] string propertyName = null)
+        {
+            var property = backingObject.GetType().GetProperty(backingPropertyName);
+            if(property == null) throw new ArgumentException(
+                $"{nameof(backingPropertyName)}, \"{backingPropertyName}\", not found on {nameof(backingObject)}.");
+
+            if (AreEqual(property.GetValue(backingObject), value)) return;
+            property.SetValue(backingObject, value, null);
+            FireEvent(propertyName);
+        }
+
+        private static bool AreEqual<T>(T field, T value) =>
+            EqualityComparer<T>.Default.Equals(field, value);
+
+        private void FireEvent(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
