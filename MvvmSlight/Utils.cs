a142f16e2ca44acfdf29502e958c5b4107c0c54c
John Burak <john.burak@gmail.com>
2018-08-24 09:24:28 -0600

    Rename for self documentation

--- MvvmSlight/Utils.cs
+++ MvvmSlight/Utils.cs
@@ -9,12 +9,7 @@ namespace MvvmSlight
 {
     public static class Utils
     {
-        /// <summary> 
-        ///   Tells if the code is running in the XAML designer. Useful for
-        ///   supplying design-time values in the actual view-model, or to
-        ///   bypass code that doesn't work in XAML designer.
-        /// </summary>
-        public static bool IsInDesignMode { get; } =
+        public static bool RunningInXamlDesigner { get; } =
             DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject());
     }
 }
